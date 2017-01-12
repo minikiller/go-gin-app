9c28a9d89ac12c8deec3dad7db649c5ef6729c0c
sunlingfeng <minikiller@qq.com>
2017-01-12 20:42:18 +0800

    refactor

--- routes.go
+++ routes.go
@@ -2,14 +2,19 @@
 
 package main
 
+import (
+	"go-gin-app/middleware"
+	"go-gin-app/handlers"
+)
+
 func initializeRoutes() {
 
 	// Use the setUserStatus middleware for every route to set a flag
 	// indicating whether the request was from an authenticated user or not
-	router.Use(setUserStatus())
+	router.Use(middleware.SetUserStatus())
 
 	// Handle the index route
-	router.GET("/", showIndexPage)
+	router.GET("/", handlers.ShowIndexPage)
 
 	// Group user related routes together
 	userRoutes := router.Group("/u")
@@ -17,39 +22,39 @@ func initializeRoutes() {
 		// Handle the GET requests at /u/login
 		// Show the login page
 		// Ensure that the user is not logged in by using the middleware
-		userRoutes.GET("/login", ensureNotLoggedIn(), showLoginPage)
+		userRoutes.GET("/login", middleware.EnsureNotLoggedIn(), handlers.ShowLoginPage)
 
 		// Handle POST requests at /u/login
 		// Ensure that the user is not logged in by using the middleware
-		userRoutes.POST("/login", ensureNotLoggedIn(), performLogin)
+		userRoutes.POST("/login", middleware.EnsureNotLoggedIn(), handlers.PerformLogin)
 
 		// Handle GET requests at /u/logout
 		// Ensure that the user is logged in by using the middleware
-		userRoutes.GET("/logout", ensureLoggedIn(), logout)
+		userRoutes.GET("/logout", middleware.EnsureLoggedIn(), handlers.Logout)
 
 		// Handle the GET requests at /u/register
 		// Show the registration page
 		// Ensure that the user is not logged in by using the middleware
-		userRoutes.GET("/register", ensureNotLoggedIn(), showRegistrationPage)
+		userRoutes.GET("/register", middleware.EnsureNotLoggedIn(), handlers.ShowRegistrationPage)
 
 		// Handle POST requests at /u/register
 		// Ensure that the user is not logged in by using the middleware
-		userRoutes.POST("/register", ensureNotLoggedIn(), register)
+		userRoutes.POST("/register", middleware.EnsureNotLoggedIn(), handlers.Register)
 	}
 
 	// Group article related routes together
 	articleRoutes := router.Group("/article")
 	{
 		// Handle GET requests at /article/view/some_article_id
-		articleRoutes.GET("/view/:article_id", getArticle)
+		articleRoutes.GET("/view/:article_id", handlers.GetArticle)
 
 		// Handle the GET requests at /article/create
 		// Show the article creation page
 		// Ensure that the user is logged in by using the middleware
-		articleRoutes.GET("/create", ensureLoggedIn(), showArticleCreationPage)
+		articleRoutes.GET("/create", middleware.EnsureLoggedIn(), handlers.ShowArticleCreationPage)
 
 		// Handle POST requests at /article/create
 		// Ensure that the user is logged in by using the middleware
-		articleRoutes.POST("/create", ensureLoggedIn(), createArticle)
+		articleRoutes.POST("/create",middleware.EnsureLoggedIn(), handlers.CreateArticle)
 	}
 }
